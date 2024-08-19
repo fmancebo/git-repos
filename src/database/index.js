24ccee04df6b261e30bcfe393aa48e2144ba0e67
fmancebo <f.92xavier1@gmail.com>
2024-08-19 16:28:12 -0300

    Configuração de porta ajustada para ambiente

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import config from "../config/database";
+// Carregue as variáveis de ambiente do arquivo .env
+dotenv.config();
 
 class Database {
   constructor() {
-    this.connection = mongoose.connect(config.url, {});
+    // Use a URL do MongoDB Atlas definida na variável de ambiente
+    const mongoURI = process.env.MONGODB_URI;
+    this.connection = mongoose
+      .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("Conectado ao MongoDB Atlas"))
+      .catch(err => console.error("Erro ao conectar ao MongoDB Atlas", err));
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
 import App from "./app";
 
-App.listen(5000);
+// Use process.env.PORT para a porta fornecida pela plataforma de hospedagem
+const PORT = process.env.PORT || 5000; // 5000 é a porta padrão usada em desenvolvimento
+
+App.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
